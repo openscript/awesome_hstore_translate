9678740e99ec5edd2574d44792ec7232721b00cf
off <offfffz@gmail.com>
2019-08-08 13:41:35 +0700

    prevent undefined #translated_attribute_names on has_many:through: query

--- lib/awesome_hstore_translate/active_record/query_methods.rb
+++ lib/awesome_hstore_translate/active_record/query_methods.rb
@@ -58,6 +58,8 @@ module AwesomeHstoreTranslate
       private
 
       def translated_attributes(opts)
+        return opts unless self.respond_to?(:translated_attribute_names)
+
         opts
           .select{ |key, _| self.translated_attribute_names.include?(key) }
           .map{ |key, value| [get_column_name(key), value] }
@@ -65,6 +67,8 @@ module AwesomeHstoreTranslate
       end
 
       def untranslated_attributes(opts)
+        return opts unless self.respond_to?(:translated_attribute_names)
+
         opts.reject{ |key, _| self.translated_attribute_names.include?(key) }
       end
     end
